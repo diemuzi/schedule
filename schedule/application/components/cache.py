a86971bb5a092d0a17cf0312382189b028de724d
Sammie S. Taunton <diemuzi@gmail.com>
2020-06-21 10:10:14 -0500

    Updating versions

--- schedule/application/components/cache.py
+++ schedule/application/components/cache.py
@@ -6,8 +6,7 @@ Cache
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': [
-            '127.0.0.1:11211'
-        ]
+        'LOCATION': [],
+        'OPTIONS': {}
     }
 }
